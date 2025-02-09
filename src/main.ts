7d8d68affec700ad86dde5844598b6fb09fbcc7e
長岡聡太 <>
2025-02-09 17:30:43 +0900

    デバック用コード削除

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,3 @@ const app = new Hono();
 app.route("/accounts", accounts);
 
 Deno.serve({ port: 8000 }, app.fetch);
-
-console.log(Deno.env.get("DB_HOST"))
-console.log(Deno.env.get("DB_USER"))
-console.log(Deno.env.get("DB_PASS"))
-console.log(Deno.env.get("DB_SCHEMA"))
