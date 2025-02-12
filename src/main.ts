21c0162d3e74c68de216dd8462d452bf6b2a6962
a163236 <nagakeita12@gmail.com>
2025-02-12 23:06:44 +0900

    dev用にCORSを全て許可

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const app = new Hono();
 
 // CORSミドルウェアの適用
 app.use('*', cors({
-    origin: 'https://soto-ui.vercel.app',
+    origin: '*',
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization'],
     maxAge: 600,
