c80aaa8c7698d01d214be27290094d07708c0792
長岡聡太 <>
2025-02-15 16:14:15 +0900

    VOを導入

--- src/domain/model/account.ts
+++ src/domain/model/account.ts
@@ -1,8 +1,16 @@
+import { AccountId } from "../valueObjects/accountId.ts";
+import { AccountName } from "../valueObjects/accountName.ts";
+import { Balance } from "../valueObjects/balance.ts";
+
 export class Account {
-  accountId: number;
-  accountName: string;
-  balance: number;
-  constructor(accountId: number, accountName: string, balance: number) {
+  accountId: AccountId;
+  accountName: AccountName;
+  balance: Balance;
+  constructor(
+    accountId: AccountId,
+    accountName: AccountName,
+    balance: Balance,
+  ) {
     this.accountId = accountId;
     this.accountName = accountName;
     this.balance = balance;
---
+++ src/domain/valueObjects/accountId.ts
@@ -0,0 +1,6 @@
+export class AccountId {
+  value: number;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
